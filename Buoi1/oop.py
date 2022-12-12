69c0985c5b239012c83a70e0f938f4bc34bcf16c
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-12-12 21:25:10 +0700

    oop-1

---
+++ Buoi1/oop.py
@@ -0,0 +1,26 @@
+class Person():
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def sleep(self):
+        print("Person sleep")
+    def eat(self):
+        print("Person eat")
+
+class BKCAD():
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def study(self):
+        print("BKACAD study")
+    def eat(self):
+        print("BKACAD eat")
+
+class Python0511(Person,BKCAD):  #Kế thừa
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+hocvien1 = Python0511("Minh",20)
+hocvien1.sleep()
+    
