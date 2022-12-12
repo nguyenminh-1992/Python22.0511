33721ea45885a3fa19668c9f8d41c7db73afb103
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-12-12 21:06:29 +0700

    class1

---
+++ Buoi1/pythonclass.py
@@ -0,0 +1,19 @@
+#Var
+#Array (List, Tuple, Set, Dict)
+#Person1 (Doi tuong) - (Thuoc tinh)
+#Function
+#Class 
+# O O P: Object Oriented Programming (Lap trinh huong doi tuong)
+
+class Python0511():
+    def __init__(self,name, age, country):
+        self.name = name
+        self.age = age
+        self.country = country
+
+
+hocvien1 = Python0511("Minh",20,"HN")
+hocvien2 = Python0511("Hai",21,"HCM")
+hocvien3 = Python0511("Tuan",22,"DN")
+
+print(hocvien1.name)
