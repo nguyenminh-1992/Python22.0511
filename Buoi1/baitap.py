47d65b1db9369b989f2715cc868fa2f49a15dd8b
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-12-12 21:32:07 +0700

    baitap

---
+++ Buoi1/baitap.py
@@ -0,0 +1,9 @@
+#Viết chương trình nhập thông tin của 1 lớp
+#Bước 1: Nhập số học viên 
+#Bước 2: Điền thông tin từng học viên (Sử dụng input)
+#Bước 3: Hiển thị danh sách sau khi nhập
+
+
+#Gợi ý: Tạo Class
+#Khai báo List
+#Dùng For để in ra
