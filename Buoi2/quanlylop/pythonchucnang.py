23faa707e36c4814fc5e46c324de735f1ffe0a89
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-12-14 21:24:41 +0700

    folder quanlylop -2

--- Buoi2/quanlylop/pythonchucnang.py
+++ Buoi2/quanlylop/pythonchucnang.py
@@ -1,1 +1,28 @@
 # TAO CAC CHUC NANG LOP PYTHON
+from pythonclass import Python
+
+class Quanlylop:
+    listhocvien = []
+    
+    def create_hoc_vien(self):
+        n = int(input("Nhap so hoc vien: "))
+        i = 0
+        while (i < n):
+            i += 1 #Lap vo han
+            ten = input("Nhap ten: ")
+            tuoi = int(input("Nhap tuoi: "))
+            hocvien = Python(ten,tuoi)
+            self.listhocvien.append(hocvien)
+
+    def show_hoc_vien(self):
+        for i in self.listhocvien:
+            print(i.name,i.age,sep=" - ")
+
+
+    # def update_hoc_vien(self):
+
+
+    # def delete_hoc_vien(self):
+
+
+
--- Buoi2/quanlylop/pythonclass.py
+++ Buoi2/quanlylop/pythonclass.py
@@ -1,6 +1,10 @@
 # DINH NGHIA VE LOP PYTHON
 
 class Python:
-    def __init__(self,id,name, age,country,english,information):
+    def __init__(self,id,name,age,country,english,information):
+        self.id = id
         self.name = name
         self.age = age
+        self.country = country
+        self.english = english
+        self.information = information
