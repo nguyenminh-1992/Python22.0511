78e65e5ea351809969748379927583d70c580410
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-12-21 20:26:02 +0700

    diem trung binh + hoc luc

--- Buoi5/quanlylop/pythonchucnang.py
+++ Buoi5/quanlylop/pythonchucnang.py
@@ -15,15 +15,24 @@ class Quanlylop:
             id = self.listhocvien.__len__() + 1
             ten = input("Nhap ten: ")
             tuoi = int(input("Nhap tuoi: "))
-            quequan = input("Nhap que quan: ")
+            quequan = input("Nhap que quan: ")         
             tienganh = float(input("Nhap diem tieng Anh: "))
             tinhoc = float(input("Nhap diem tin hoc: "))
             hocvien = Python(id,ten,tuoi,quequan,tienganh,tinhoc)
+            
+            hocvien.diemtrungbinh = float((tienganh + tinhoc)/2)
+            if hocvien.diemtrungbinh <4 :
+                hocvien.hocluc = "Yeu"      
+            elif hocvien.diemtrungbinh < 7:
+                hocvien.hocluc = "Kha"
+            else:
+                hocvien.hocluc = "Gioi"   
+                        
             self.listhocvien.append(hocvien)
 
     def show_hoc_vien(self):
         for i in self.listhocvien:
-            print(i.id,i.name,i.age,sep=" - ")
+            print(i.id,i.name,i.age,i.country,i.english,i.information,i.diemtrungbinh,i.hocluc,sep=" - ")
 
 
     def update_hoc_vien(self,id1):
