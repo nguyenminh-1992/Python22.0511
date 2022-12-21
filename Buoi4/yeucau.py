38d35702aaeb24a9384b166cc375da960c604b31
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-12-21 19:59:15 +0700

    buoi4

--- Buoi4/yeucau.py
+++ Buoi4/yeucau.py
@@ -1,3 +1,4 @@
 #ID tự động tăng (1,2,3,4,5,...)
-#Sắp xếp theo tên
+# Thêm chức năng sắp xếp theo tên (sort)
 # Thêm cột điểm trung bình + đánh giá (0->4 yeu, 4 ->7 Kha, 7 -> 10 Giỏi)
+# khi show học viên
