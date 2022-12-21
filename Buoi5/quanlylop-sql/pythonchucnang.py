8e86f1478e3f5b8df643279c2dd79096712b4d1f
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-12-21 21:55:26 +0700

    get data from sql

--- Buoi5/quanlylop-sql/pythonchucnang.py
+++ Buoi5/quanlylop-sql/pythonchucnang.py
@@ -1,7 +1,23 @@
 import pythonclass
 
+#Ket noi giua MySQL va Python
 ketnoi = pythonclass.getConnection()
-print(ketnoi)
+#print(ketnoi)
+
+#C R U D
+dulieu = ketnoi.cursor()
+
+def get_data():
+    dulieu.execute("SELECT * FROM Quan_ly_hoc_vien.Hocvien")
+    ketqua = dulieu.fetchall()
+    for i in ketqua:
+        print(i)
+
+
+get_data()
+    
+
+
 
 
 
