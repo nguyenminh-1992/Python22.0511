c67d1dd2c6e807d4fdd03afe37a39b6c31fb5e30
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2022-12-21 21:34:04 +0700

    connection-sql

---
+++ Buoi5/quanlylop-sql/pythonclass.py
@@ -0,0 +1,12 @@
+# DINH NGHIA VE LOP PYTHON
+
+import mysql.connector
+
+def getConnection():
+    khaibao = mysql.connector.connect(host='localhost',
+                                    user='root',
+                                    passwd='root',
+                                    database='Quan_ly_hoc_vien')
+    return khaibao
+
+#pip3 install mysqlconnector
---
+++ Buoi5/quanlylop-sql/pythonmain.py
@@ -0,0 +1,44 @@
+#THUC THI CAC CHUC NANG
+
+#MENU
+
+from pythonchucnang import *
+
+hocvien = Quanlylop()
+
+while(True):
+    print("-------------------------------")
+    print("1. Them hoc vien")
+    print("2. Hien thi hoc vien")
+    print("3. Sua thong tin hoc vien")
+    print("4. Xoa thong tin hoc vien")
+    print("5. Sap xep thong tin hoc vien")
+    print("-------------------------------")
+
+    print("Moi ban chon chuc nang")
+
+    nhap = int(input("Chon chuc nang: "))
+    if (nhap == 1):
+        hocvien.create_hoc_vien()
+    elif (nhap == 2):
+        hocvien.show_hoc_vien()
+    elif (nhap ==3):
+        id1 = int(input("Nhap id muon sua: "))
+        hocvien.update_hoc_vien(id1)
+        print("Da sua thanh cong")
+    elif (nhap ==4):
+        id2 = int(input("Nhap id muon xoa: "))
+        hocvien.delete_hoc_vien(id2)
+        print("Da xoa thanh cong")
+    elif (nhap ==5):
+        hocvien.sapxep_hoc_vien()
+        hocvien.show_hoc_vien()
+    else:
+        break
+
+# elif (nhap == 3):
+
+# elif (nhap == 4):
+
+# else:
+#     print("Nhap sai vui long nhap lai")
