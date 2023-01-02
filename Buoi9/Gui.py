9dc264660ffaaa24bb03f7dae8ee34a78d9a7525
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2023-01-02 20:04:35 +0700

    label-textbox-button

--- Buoi9/Gui.py
+++ Buoi9/Gui.py
@@ -6,7 +6,15 @@ giaodien = tkinter.Tk()
 giaodien.title("Chuong trinh")
 giaodien.geometry('500x400')
 
-
+#Label
+label = tkinter.Label(giaodien,text="Xin chao",fg="black",bg="white")
+label.grid(column=1,row=0)
+#Textbox
+textbox = tkinter.Entry(giaodien,width=30)
+textbox.grid(column=2,row=0)
+#Button
+button = tkinter.Button(giaodien,text="Click me",fg="black",bg="white")
+button.grid(column=2,row=1)
 
 giaodien.mainloop()
 
